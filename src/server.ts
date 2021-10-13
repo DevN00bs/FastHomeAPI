2bf37d6998302c8dead21dea0d7529fa800ddc4c
Brandom Rodriguez <brandomrobor@tuta.io>
2021-10-13 17:20:35 -0600

    Added method to test database connection Database configuration should be loaded using the .env file

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
+import pool from "./db/pool";
 
+/**
+ * Temporal function to verify database connectivity
+ */
+
+async function testDatabase() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT * FROM Properties");
+    return rows
+  } catch (e) {
+    return { message: "Database connection failed ", err: e }
+  }
+}
+
+testDatabase().then((res) => {
+  console.dir(res.meta ? "Success!" : res)
+})
