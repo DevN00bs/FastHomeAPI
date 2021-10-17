98d033c7c5a930bbe6598bc07273d4893f1350fe
chacon <chaconlealj@gmail.com>
2021-10-17 13:31:45 -0600

    Creation of the routes and integration of cors.
    I had troubles with the endpoints of the properties, they had interferience (line 59-71)
     when I tried to use the different /:xxxid's, thats why they only have numbers. Hope you can
    explain me why, or how to fix it.
    
    All the others endpoints are working fine, Hope all of them are correct.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import expressJSDocSwagger from "express-jsdoc-swagger";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +28,118 @@ expressJSDocSwagger(app)({
       description: "Used for testing",
     },
   ],
+});
+
+app.use(cors());
+
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Contet-Type");
+  next();
+});
+
+//login.
+
+app.post('/v1/login', (req, res) => {
+  console.log("log in");
+  res.send('log in')
+});
+
+app.post('/v1/register', (req, res) => {
+  console.log("register");
+  res.send('register')
+});
+
+//Properties.
+app.get('/v1/properties', (req,res)=>{
+  res.send('Show all properties')
+});
+
+app.get('/v1/properties/1',(req,res) =>{
+  res.send('Property with id');
+});
+
+app.get('/v1/properties/2', (req,res)=>{
+  res.send('Vendor info ');
+})
+
+app.get('/v1/properties/3', (req,res)=>{
+  res.send('available');
+})
+
+app.get('/v1/properties/4', (req,res)=>{
+  res.send('price')
+})
+
+app.post("/v1/properties", (req, res) => {
+  console.log("new property added");
+  res.send('added')
+});
+
+app.put("/v1/properties/:propertyId", (req, res) => {
+  console.log("Edited succesfully.");
+  res.send("Edited succesfully.");
+});
+
+app.delete("/v1/properties/:propertyId", (req, res) => {
+  console.log("Property deleted");
+  res.send("deleted");
+});
+
+//Profiles.
+app.get("/v1/profiles", (req, res) => {
+  console.log("all vendors");
+  res.send('vendors')
+});
+
+app.post("/v1/profiles/:userId", (req, res) => {
+  console.log("SHow reviews?");
+  res.send('show reviews of this user')
+});
+
+// doubts
+app.post("/v1/profiles/:userId/:reviewId", (req, res) => {
+  console.log("show one review?");
+  res.send('show one review')
+});
+
+app.post("/v1/profiles/reviews", (req, res) => {
+  console.log("Review Created?");
+});
+
+app.put("/v1/profiles/reviews/:reviewId", (req, re) => {
+  console.log("review updated?");
+});
+///// doubts
+app.post("/v1/profiles/:userId/details", (req, res) => {
+  console.log("details card");
+  res.send("details card of this user id");
+});
+
+app.post("/v1/profiles/:userId/newDetails", (req, res) => {
+  console.log("Detail added");
+  res.send("Details added");
+});
+
+app.put("/v1/profiles/:userId/details", (req, res) => {
+  console.log("details updated");
+  res.send("details updated");
+});
+
+app.delete("/v1/profiles/:userId/details", (req, res) => {
+  console.log("details deleted ");
+  res.send("details deleted");
+});
+
+app.put("/v1/profiles/:userId", (req, res) => {
+  console.log("User edited");
+  res.send("edited");
+});
+
+app.delete("v1//profiles/:userId", (req, res) => {
+  console.log("Profile Deleted");
+  res.send("deleted");
 });
 
 app.listen(port, () => {
