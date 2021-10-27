540d04f640576d6f7513cb4075c2d562beb11f1e
Brandom Rodriguez <brandomrobor@tuta.io>
2021-10-27 15:45:12 -0600

    Added email verification system At the moment we send an ugly, unformatted message. We plan to replace it with a HTML message with the project's logo

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,6 +7,8 @@ import {
 import pool from "../db/pool";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import mailer from "../db/email";
+import crypto from "crypto";
 
 export async function createUser(
   data: RegistrationData
@@ -74,6 +76,64 @@ export async function loginUser(
         token: jwt.sign(userData[0].userId, process.env.SECRET!),
       },
     };
+  } catch (error) {
+    console.error("Something went wrong", error);
+    return { isSuccessful: false };
+  } finally {
+    conn?.release();
+  }
+}
+
+export async function sendVerifyMail(
+  username: string
+): Promise<ControllerResponse<void>> {
+  let conn;
+
+  try {
+    conn = await pool.getConnection();
+    const userData = await conn.query(
+      "SELECT userId, email FROM Users WHERE username = ?",
+      [username]
+    );
+
+    await mailer.sendMail({
+      from: '"FastHome" <' + process.env.MAIL_USER + ">",
+      to: userData[0].email,
+      subject: "Verify your email address",
+      text:
+        username +
+        ", welcome to FastHome!\nVerify your account with this link:\nhttp://localhost:5000/verify/" +
+        userData[0].userId +
+        crypto.createHash("md5").update(userData[0].email).digest("hex"),
+    });
+
+    return { isSuccessful: true };
+  } catch (error) {
+    console.error("Something went wrong", error);
+    return { isSuccessful: false };
+  } finally {
+    conn?.release();
+  }
+}
+
+export async function verifyMail(
+  id: string
+): Promise<ControllerResponse<boolean>> {
+  let conn;
+
+  if (id.length < 33) {
+    return { isSuccessful: true, result: false };
+  }
+
+  const parsedId = parseInt(id.substr(0, id.length - 32));
+
+  try {
+    conn = await pool.getConnection();
+    const query = await conn.query(
+      "UPDATE Users SET emailVerified = 1 WHERE userId = ?",
+      [parsedId]
+    );
+    return { isSuccessful: true, result: query.affectedRows === 1 };
   } catch (error) {
     console.error("Something went wrong", error);
     return { isSuccessful: false };
