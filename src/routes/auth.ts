6847677668571052ed1c1dc97683655ae08a93fa
Brandom Rodriguez <brandomrobor@tuta.io>
2021-11-08 14:33:15 -0700

    Added validation to registration route Next commit this implementation will be converted to a middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,26 @@
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 import { Router } from "express";
 import { createUser } from "../controllers/auth";
+import { RegistrationData } from "../entities/auth";
 
 const router = Router();
 
 router.post("/register", async (req, res) => {
+  const data = plainToClass(RegistrationData, req.body);
+  const errors = await validate(data);
+
+  if (errors.length > 0) {
+    return res.status(400).json({
+      invalid: errors
+        .filter((error) => error.value !== undefined)
+        .map((error) => error.property),
+      missing: errors
+        .filter((error) => error.value === undefined)
+        .map((error) => error.property),
+    });
+  }
+
   const creation = await createUser(req.body);
 
   if (!creation.isSuccessful) {
