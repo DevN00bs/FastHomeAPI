c21a26c9409c850008d3ac07efd799c5d10280ef
Brandom Rodriguez <brandomrobor@tuta.io>
2021-11-08 15:05:05 -0700

    Moved validation logic to a middleware Now we can validate any class in any route, you just need to provide the type and we return the parsed data in the res.locals object

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,27 +1,12 @@
-import { plainToClass } from "class-transformer";
-import { validate } from "class-validator";
 import { Router } from "express";
 import { createUser } from "../controllers/auth";
 import { RegistrationData } from "../entities/auth";
+import validation from "../middleware/validation";
 
 const router = Router();
 
-router.post("/register", async (req, res) => {
-  const data = plainToClass(RegistrationData, req.body);
-  const errors = await validate(data);
-
-  if (errors.length > 0) {
-    return res.status(400).json({
-      invalid: errors
-        .filter((error) => error.value !== undefined)
-        .map((error) => error.property),
-      missing: errors
-        .filter((error) => error.value === undefined)
-        .map((error) => error.property),
-    });
-  }
-
-  const creation = await createUser(req.body);
+router.post("/register", validation(RegistrationData), async (req, res) => {
+  const creation = await createUser(res.locals.data);
 
   if (!creation.isSuccessful) {
     return res.sendStatus(500);
