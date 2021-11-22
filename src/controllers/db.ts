d0b0ae4e33506f016fd863e63249175c0b555a36
Brandom Rodriguez <brandomrobor@tuta.io>
2021-11-22 13:21:25 -0700

    Remade the insert query generator using JS features Yeah, this simplifies things a lot

--- src/controllers/db.ts
+++ src/controllers/db.ts
@@ -1,37 +1,9 @@
 export function createInsertQuery(table: string, object: object): string {
-  let query = `INSERT INTO ${table} (`;
+  const parsedValues = Object.values(object).map((value) =>
+    typeof value === "string" ? `'${value}'` : value.toString()
+  );
 
-  const columns = Object.keys(object);
-  const values = Object.values(object);
-
-  columns.forEach((column, index) => {
-    query += `\`${column}\``;
-
-    if (index !== columns.length - 1) {
-      query += ",";
-    }
-  });
-
-  query += ") VALUES (";
-
-  values.forEach((value, index) => {
-    switch (typeof value) {
-      case "number":
-        query += value.toString();
-        break;
-      case "string":
-        query += `'${value}'`;
-        break;
-      default:
-        console.error(
-          "Excuse me, this type is not implemented yet. " + typeof value
-        );
-    }
-
-    if (index !== columns.length - 1) {
-      query += ",";
-    }
-  });
-
-  return query + ")";
+  return `INSERT INTO ${table} (${Object.keys(
+    object
+  )}) VALUES (${parsedValues})`;
 }
