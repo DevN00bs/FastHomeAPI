af5b2602a6629895caf1e1ea475ea5fb298d3042
Brandom Rodriguez <brandomrobor@tuta.io>
2021-11-23 14:55:53 -0700

    Documented catalog data route

---
+++ src/entities/misc.ts
@@ -0,0 +1,7 @@
+/**
+ * @typedef {object} CatalogsData
+ * @description Contains the IDs used in the database catalogs
+ * @property {array<string>} currencies - The list of currencies available in the database
+ * @property {array<string>} contracts - The list of contracts available in the database
+ */
+class CatalogsData {}
--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -1,16 +1,36 @@
 import { Router } from "express";
 import { getCatalogsData } from "../controllers/misc";
 
-const router = Router()
+const router = Router();
 
+/**
+ * GET /api/misc/catalogs
+ * @tags Miscellaneous
+ * @summary Get the available data directly from the database
+ * @return {CatalogsData} 200 - An object with all the catalogs available
+ * @return 500 - Internal Server Error. If you see this ever, please tell us in the group
+ * @example response - 200 - Catalogs data
+ * {
+ *  "currencies": [
+ *    "---",
+ *    "Mexican Peso - MXN",
+ *    "US Dollars - USD"
+ *  ],
+ *  "contracts": [
+ *    "---",
+ *    "Purchase",
+ *    "Lease"
+ *  ]
+ * }
+ */
 router.get("/catalogs", async (_req, res) => {
-  const catalogs = await getCatalogsData()
+  const catalogs = await getCatalogsData();
 
   if (!catalogs.isSuccessful) {
-    return res.sendStatus(500)
+    return res.sendStatus(500);
   }
 
-  res.json(catalogs.result)
-})
+  res.json(catalogs.result);
+});
 
-export default router
+export default router;
