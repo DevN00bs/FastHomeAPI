153584b20a2409b55f60ef901a1b779f42fe860f
Brandom Rodriguez <brandomrobor@tuta.io>
2021-11-24 14:20:50 -0700

    Added filename callback to multer to avoid duplicates

--- src/conf/images.ts
+++ src/conf/images.ts
@@ -1,10 +1,15 @@
 import multer from "multer";
 import GCS from "multer-cloud-storage";
+import crypto from "crypto";
 
 const upload = multer({
   storage: new GCS({
     uniformBucketLevelAccess: false,
     acl: "publicRead",
+    filename: (_req: Express.Request, file: Express.Multer.File, cb: any) => {
+      const parsedFile = file.originalname.split(".");
+      cb(null, `${crypto.randomUUID()}.${parsedFile[parsedFile.length - 1]}`);
+    },
   }),
 });
 
